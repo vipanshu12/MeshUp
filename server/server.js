c271f7c74df5473292cb751235257d2393e4f6b7
vipanshu12 <vgupta48030@gmail.com>
2026-04-01 20:46:30 +0530

    Fix response format for root route

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cors())
 
 // Routes
-app.get('/', (req, res) => res.send({ message: 'Server is running' }))
+app.get('/', (req, res) => res.send( 'Server is running'))
 app.use('/api/inngest', serve({client: inngest, functions}))
 
 // app.get('/home', (req, res) => res.send('Server is running'))
